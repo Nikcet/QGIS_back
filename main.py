52cf7622789d1d62d3a45c4ce118d856bca8a947
nikcet <mstislav.kr@yandex.ru>
2025-07-23 00:39:47 +0300

    add template

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from contextlib import asynccontextmanager
+from sqlmodel import Session
 
 from app import logger
 from app.endpoints import router
-from app.database import engine
+from app.database import engine, get_feature_stats, get_all_features
+from app.dependencies import get_session
 from app.models import SQLModel
+import os
 
 app = FastAPI()
 
 app.include_router(router)
+
+templates = Jinja2Templates(
+    directory=os.path.join(os.path.dirname(__file__), "templates")
+)
+
+
+@app.get("/dashboard", response_class=HTMLResponse)
+def dashboard(request: Request, session: Session = Depends(get_session)):
+    stats = get_feature_stats(session)
+    features = get_all_features(session)[-10:]  # последние 10 объектов
+    return templates.TemplateResponse(
+        "dashboard.html", {"request": request, "stats": stats, "features": features}
+    )
 
 
 @asynccontextmanager
@@ -18,5 +36,5 @@ async def lifespan(app: FastAPI):
     yield
     logger.warning("Servir is shutting down...")
 
-app = FastAPI(lifespan=lifespan)
 
+app = FastAPI(lifespan=lifespan)
