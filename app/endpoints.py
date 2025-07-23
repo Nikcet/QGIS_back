6974108517bf3dc28356e40553a00855e924287e
Nikcet <mstislav.kr@yandex.ru>
2025-07-24 00:11:26 +0300

    .

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,7 +1,13 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlmodel import Session
 import json
+import os
 
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, Depends, HTTPException, Request
+from sqlmodel import Session, select
+
+from app import logger
+from app.models import Feature as FeatureModel
 from app.dependencies import get_session
 from app.schemas import FeatureCreate
 from app.database import (
@@ -16,9 +22,15 @@ router = APIRouter()
 
 @router.post("/features")
 def add_feature(feature: FeatureCreate, session: Session = Depends(get_session)):
+
+    existing = session.exec(select(FeatureModel).where(FeatureModel.geometry == str(feature.geometry))).first()
+    if existing: return 
+
     db_feature = create_feature(
         session, geometry=str(feature.geometry), type_=feature.type
     )
+
+    logger.info(f"Post db feature: {db_feature}")
     return {"id": db_feature.id}
 
 
@@ -52,3 +64,15 @@ def delete_feature(feature_id: int, session: Session = Depends(get_session)):
 def get_stats(session: Session = Depends(get_session)):
     stats = get_feature_stats(session)
     return stats
+
+
+@router.get("/dashboard", response_class=HTMLResponse)
+def dashboard(request: Request, session: Session = Depends(get_session)):
+    templates = Jinja2Templates(
+        directory=os.path.join(os.path.dirname(__file__), "templates")
+    )
+    stats = get_feature_stats(session)
+    features = get_all_features(session)[-10:]  # последние 10 объектов
+    return templates.TemplateResponse(
+        "dashboard.html", {"request": request, "stats": stats, "features": features}
+    )
