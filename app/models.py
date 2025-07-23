ea56db0deda1bcdaf0174e84d7e5c9cad5e0716e
Nikcet <mstislav.kr@yandex.ru>
2025-07-24 00:11:47 +0300

    change type of id field

--- app/models.py
+++ app/models.py
@@ -4,6 +4,6 @@ from shortuuid import uuid
 
 
 class Feature(SQLModel, table=True):
-    id: str = Field(default_factory=lambda: uuid(), primary_key=True)
+    id: Optional[str] = Field(default_factory=lambda: uuid(), primary_key=True)
     geometry: str  # GeoJSON как строка
     type: str  # Point, LineString, Polygon
